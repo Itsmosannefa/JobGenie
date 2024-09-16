98ba40e2bc363011e1043978defb5b26a1ea13ad
Parthivthadani <thadaniparthiv2@gmail.com>
2024-09-16 20:07:53 +0530

    Revert "swagger register & login schema and api created"
    This reverts commit 5575dcb5ee24d404e9768f06a158691642b4d440.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -15,113 +15,47 @@ const router = express.Router()
 // routes
 
 /**
- * @swagger
+ *  @swagger
  * components:
  *  schemas:
- *    User:
- *      type: object
- *      required:
- *        - name
- *        - lastName
- *        - email
- *        - password
- *      properties:
- *        id:
- *          type: string
- *          description: The Auto-generated id of user collection
- *          example : DHSASDHJDJHVAJDSVJAVSD
- *        name:
- *          type: string
- *          description: User name
- *        lastName:
- *          type: string
- *          description: User Last Name
- *        email:
- *          type: string
- *          description: user email address
- *        password:
- *          type: string
- *          description: user password should be greater then 6 character
- *        location:
- *          type: string
- *          description: user location city or country
- *      example:
- *        id: GDHJGD788BJBJ
- *        name: tony
- *        lastName: stark
- *        email: ironman@gmail.com
- *        password: ironman@123
- *        location: newyork
+ *      user:
+ *         type:Object
+ *         required:
+ *             -name
+ *             -lastname
+ *             -email
+ *             -password
+ *             -location
+ *          properties:
+ *             id:
+ *                type: string
+ *                description: The Auto-generated id of collection
+ *             name:
+ *                type: string
+ *                description: User name
+ *             lastname:
+ *                type: string
+ *                description: User lastname
+ *             email:
+ *                type: string
+ *                description: User email-address  
+ *             password:
+ *                type: string
+ *                description: User password should be greater than 6 character
+ *             location:
+ *                type: string
+ *                description: User location city or country
+ *             example:
+ *                 id: GDHJG&788BJBJ
+ *                 name: tony
+ *                 lastname: stark
+ *                 email: tonystark@gamil.com
+ *                 password: ironman
+ *                 location: new york
  */
-
-
-
-/**
- *  @swagger
- *  tags :
- *     name: Auth
- *     description: authentication apis
- * 
- */
-
-/**
- *  @swagger
- *  tags:
- *    name: Auth
- *    description: authentication apis
- */
-
-/**
- * @swagger
- * /api/v1/auth/register:
- *    post:
- *      summary: register new user
- *      tags: [Auth]
- *      requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/User'
- *      responses:
- *        200:
- *          description: user created successfully
- *          content:
- *            application/json:
- *              schema:
- *                $ref: '#/components/schemas/User'
- *        500:
- *          description: internal serevr error
- */
-
-
 
 // REGISTER || POST
 router.post('/register', limiter, registerController);
-
-
-/**
- * @swagger
- * /api/v1/auth/login:
- *  post:
- *    summary: login page
- *    tags: [Auth]
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/User'
- *    responses:
- *      200:
- *        description: login successfull
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/User'
- *      500:
- *        description: something went wrong
- */
 
 // LOGIN || POST
 router.post('/login', limiter, loginController);
