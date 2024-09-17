7d8504da6087361170f721fc7cf1172e80beeaa0
Parthivthadani <thadaniparthiv2@gmail.com>
2024-09-17 19:33:21 +0530

    remove swagger

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,45 +14,6 @@ const router = express.Router()
 
 // routes
 
-/**
- *  @swagger
- * components:
- *  schemas:
- *      user:
- *         type:Object
- *         required:
- *             -name
- *             -lastname
- *             -email
- *             -password
- *             -location
- *          properties:
- *             id:
- *                type: string
- *                description: The Auto-generated id of collection
- *             name:
- *                type: string
- *                description: User name
- *             lastname:
- *                type: string
- *                description: User lastname
- *             email:
- *                type: string
- *                description: User email-address  
- *             password:
- *                type: string
- *                description: User password should be greater than 6 character
- *             location:
- *                type: string
- *                description: User location city or country
- *             example:
- *                 id: GDHJG&788BJBJ
- *                 name: tony
- *                 lastname: stark
- *                 email: tonystark@gamil.com
- *                 password: ironman
- *                 location: new york
- */
 
 // REGISTER || POST
 router.post('/register', limiter, registerController);
