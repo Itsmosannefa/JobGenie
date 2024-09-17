8dbfe64a0b402199e73d3d660efd4e0abfa7670a
Itsmosannefa <khanammosannefa@gmail.com>
2024-09-17 13:57:53 +0530

    modification

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -10,13 +10,13 @@ const HomePage = () => {
       </video>
       <div className="content">
         <div className="card w-25">
-          <img src="/assets/images/logo/logo.png" alt="logo" />
+          <img src="/assets/images/logo/logo.jpg" alt="logo" />
           <hr />
           <div className="card-body" style={{ marginTop: "-60px" }}>
             <h5 className="card-title">Indias No #1 Carrer Platform</h5>
             <p className="card-text">
               Search and manage your jobs with ease. free and open source job
-              ortal application by techinfoyt
+              portal application
             </p>
             <div className="d-flex justify-content-between mt-5">
               <p>
