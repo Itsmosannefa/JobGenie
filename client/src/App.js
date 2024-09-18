01fd8ef0d611a28ad94de209e2865fec222b1d8b
Parthivthadani <thadaniparthiv2@gmail.com>
2024-09-18 17:44:17 +0530

    added login and register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Login from "./pages/loginpage/login.jsx";
+import Register from "./pages/signupPage/signUp.jsx";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 import { ToastContainer } from "react-toastify";
